7dca6585ecf2970e89bd63a2c90eec179b78904f
cprtmchzh <bin54511563@gmail.com>
2023-09-18 07:49:37 +0900

    2, 4차시 과제(미완료)  임시 저장

---
+++ week_1/week_1.js
@@ -0,0 +1,12 @@
+/* 각각의 console.log의 출력 값을 알아오시오.
+console.log(a);
+var a;
+a = 11;
+console.log(a); //
+*/
+
+/* 각각의 console.log의 출력 값을 알아오시오. */
+console.log(a);
+a = 10;
+var a;
+console.log(a); //
