bd5f068941380239284b33a41c2372673c1a4c6e
cprtmchzh <bin54511563@gmail.com>
2023-09-20 14:59:41 +0900

    간단한 정적 웹페이지 구현

---
+++ week_2/week_2-buttonPage/reaction.js
@@ -0,0 +1,34 @@
+if (shape == 0) {
+    star = ['  *  ',' *** ','*****',' *** ','  *  '];
+    for(let i = 0; i < 5; i++) {
+              document.write(star[i] + '<br>');
+          }
+  }
+  
+  if (shape == 1) {
+    star = ['*****','**** ','***  ','**   ','*    '];
+    for(let i = 0; i < 5; i++) {
+              document.write(star[i] + '<br>');
+          }
+  }
+  
+  if (shape == 2) {
+    star = ['*****','****','***','**','*'];
+    for(let i = 0; i < 5; i++) {
+              document.write(star[i] + '<br>');
+          }
+  }
+  
+  if(shape == 3) {
+    star = ['*****',' *** ','  *  ',' *** ','*****'];
+    for(let i = 0; i < 5; i++) {
+              document.write(star[i] + '<br>');
+          }
+  }
+  
+  if(shape == 4) {
+    star = ['    *','   **','  ***',' ****','*****'];
+    for(let i = 0; i < 5; i++) {
+              document.write(star[i] + '<br>');
+          }
+  }
