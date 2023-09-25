725bdd97b6db7149f5a4ba55a1058a7099c3a3d0
seonbin <bin54511563@gmail.com>
2023-09-25 17:38:10 +0900

    3차시 과제 임시저장

---
+++ week_2/test.js
@@ -0,0 +1,10 @@
+//var isThereMessage = true;
+//var message ='';
+
+//if(isThereMessage) message = '멘토는 죽어있다.';
+//console.log(message); // 멘토는 죽어있다.
+
+//message = isThereMessage && '멘토는 죽어있다.';
+//console.log(message); // 멘토는 죽어있다.
+
+let checkString = Boolean("String");
