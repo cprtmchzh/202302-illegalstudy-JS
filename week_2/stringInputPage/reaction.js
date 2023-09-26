c00bd1ac210e4372e849d9a4fd0c7cf9b8e342a8
seonbin <bin54511563@gmail.com>
2023-09-26 10:15:00 +0900

    일부 파일 수정

--- week_2/stringInputPage/reaction.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByTagName("form")[0].onsubmit = function() {
-    var inputNumber = this.inputNumber.value
-    var check = '입력 성공';
-
-    check = true || '문자열이 입력되지 않았습니다';
-    alert(inputNumber);
-}
